36dc59c823020fb54de5cdc6837f668f48bcee34
kira23j <kirubelmamuyesahle@gmail.com>
2025-04-01 11:03:25 +0300

    add footer component to marketing layout

--- 02.Taskify/taskify/app/(marketing)/layout.tsx
+++ 02.Taskify/taskify/app/(marketing)/layout.tsx
@@ -1,5 +1,5 @@
 import { Navbar } from "./_components/navbar";
-
+import { Footer } from "./_components/footer";
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
     return (
 <div className="h-full bg-slate-100">
@@ -7,6 +7,7 @@ const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
 <main className="pt-40 pb-20 bg-slate-100">
 {children}
 </main>
+<Footer />
 </div>  
 );}
 export default MarketingLayout;
