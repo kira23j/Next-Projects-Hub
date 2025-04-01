58da30ba10444f494022c2bf3a7c25041b9d3454
kira23j <kirubelmamuyesahle@gmail.com>
2025-04-01 05:57:49 +0300

    add main marketing layout and page components

--- 02.Taskify/taskify/app/page.tsx
+++
@@ -1,7 +0,0 @@
-
-
-export default function Home() {
-  return (
-  <div  className="text-sky-500 flex-1">taskify</div>
-  );
-}
--- 02.Taskify/taskify/app/users/page.tsx
+++
@@ -1,7 +0,0 @@
-const IdPage = () => {
-    return (
-        <div className="text-sky-500 flex-1">id page</div>
-    )
-}
-
-export default IdPage;
